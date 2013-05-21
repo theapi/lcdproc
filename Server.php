174a9c2b03ddfc7bfca00033243cf2dc4b4a0e1d
Peter <petermclarke@gmail.com>
2013-05-21 17:31:41 +0100

    Start of listening to multiple clients

--- Server.php
+++ Server.php
@@ -7,7 +7,9 @@ class Server
   protected $ip;
   protected $port;
   protected $socket;
-  protected $clients = array();
+
+  // Hold arrays for stream_select to listen to
+  protected $streams = array();
 
   public function run($ip = '127.0.0.1', $port = 13666)
   {
@@ -19,14 +21,51 @@ class Server
       throw new \Exception('Unable to create ' . $this->ip . ':' . $this->port, $errno);
     }
 
-    // TODO: lcdproc instead of time :)
-    while ($conn = stream_socket_accept($this->socket)) {
-      fwrite($conn, 'The local time is ' . date('n/j/Y g:i a') . "\n");
-      fclose($conn);
+    $this->streams[] = $this->socket;
+    $write = null;
+    $except = null;
+
+    while (1) {
+      $changed = stream_select($this->streams, $write, $except, 200000);
+      if ($changed === FALSE) {
+        // Mmm a problem
+        break;
+      }
+
+      for ($i = 0; $i < $changed; $i++) {
+        if ($this->streams[$i] === $this->socket) {
+          // the server has something
+          $conn = stream_socket_accept($this->socket);
+          // TODO: lcdproc instead of time :)
+          fwrite($conn, "Hello! The time is ".date("n/j/Y g:i a")."\n");
+          // add the client to the array to be watched
+          $this->streams[] = $conn;
+        }
+        else {
+
+          $sock_data = fread($this->streams[$i], 1024);
+          if (strlen($sock_data) === 0) { // connection closed
+            $this->removeStream($i);
+          }
+          else if ($sock_data === FALSE) {
+            // something bad happened
+            $this->removeStream($i);
+          }
+          else {
+            fwrite($this->streams[$i], "You have sent :[".$sock_data."]\n");
+          }
+
+        }
+      }
     }
 
     fclose($this->socket);
+  }
 
+  public function removeStream($index)
+  {
+    fclose($this->streams[$index]);
+    unset($this->streams[$index]);
   }
 
 }
