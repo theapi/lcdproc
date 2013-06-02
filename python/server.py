5adcff379b86666d26694962802b0a9536482b0f
Peter Clarke <petermclarke@gmail.com>
2013-06-02 16:55:21 +0100

    The python server that actually talks to the lcd screen

---
+++ python/server.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import socket, select, atexit
+
+from time import sleep
+from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+
+
+#Function to broadcast chat messages to all connected clients
+def broadcast_data (sock, message):
+    #print message
+    lcd.clear()
+    lcd.message(message)
+    
+    try:
+        sock.send("ok\n")
+    except :
+        # broken socket connection may be, chat client pressed ctrl+c for example
+        socket.close()
+        CONNECTION_LIST.remove(socket)
+
+def goodbye():
+    lcd.clear()
+    lcd.backlight(lcd.VIOLET)
+    lcd.message("BYE")
+    sleep(.2)
+    lcd.backlight(lcd.OFF)
+    lcd.clear()
+    lcd.stop() 
+
+atexit.register(goodbye)
+
+if __name__ == "__main__":
+    
+    # Initialize the LCD plate.  Should auto-detect correct I2C bus.  If not,
+    # pass '0' for early 256 MB Model B boards or '1' for all later versions
+    lcd = Adafruit_CharLCDPlate()
+    
+    # Clear display and show greeting, pause 1 sec
+    lcd.clear()
+    lcd.message("Adafruit RGB LCD\nPlate w/Keypad!")
+
+    print "❤"     
+    # List to keep track of socket descriptors
+    CONNECTION_LIST = []
+    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
+    PORT = 8888
+     
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # this has no effect, why ?
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(("0.0.0.0", PORT))
+    server_socket.listen(1)
+ 
+    # Add server socket to the list of readable connections
+    CONNECTION_LIST.append(server_socket)
+ 
+    print "Server started on port " + str(PORT)
+    lcd.clear()
+    # no fancy utf8 like: lcd.message("❤")
+    lcd.message("Started on\nport " + str(PORT))
+    #lcd.write(u"\u2764"); 
+    while 1:
+        # Get the list sockets which are ready to be read through select
+        read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[],0.1)
+ 
+        for sock in read_sockets:
+            #New connection
+            if sock == server_socket:
+                # Handle the case in which there is a new connection recieved through server_socket
+                sockfd, addr = server_socket.accept()
+                CONNECTION_LIST.append(sockfd)
+                print "Client (%s, %s) connected" % addr
+                 
+                broadcast_data(sockfd, "%s:\n%s" % addr)
+             
+            #Some incoming message from a client
+            else:
+                # Data recieved from client, process it
+                try:
+                    #In Windows, sometimes when a TCP program closes abruptly,
+                    # a "Connection reset by peer" exception will be thrown
+                    data = sock.recv(RECV_BUFFER)
+                    if data:
+                        #data = data.decode("utf-8")
+                        broadcast_data(sock, data)                
+                 
+                except:
+                    print "Client (%s, %s) is offline" % addr
+                    sock.close()
+                    CONNECTION_LIST.remove(sock)
+                    continue
+     
+    server_socket.close()
+
+
+
+
+# Clear the screen
+lcd.clear()
+lcd.backlight(lcd.OFF)
