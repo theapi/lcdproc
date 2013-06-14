82c730fce7597317467a589f94e8592c83200c08
Peter <petermclarke@gmail.com>
2013-06-14 14:26:48 +0100

    websocket example

--- Server/Drivers/WebSocket/Server.php
+++ Server/Drivers/WebSocket/Server.php
@@ -20,10 +20,6 @@ class Server implements MessageComponentInterface {
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
         foreach ($this->clients as $client) {
             if ($from !== $client) {
                 // The sender is not the receiver, send to each client connected
