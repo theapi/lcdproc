4584fb5e6b702f1de07d828c2e2b76ac7d90e570
Peter Clarke <petermclarke@gmail.com>
2013-06-18 20:53:03 +0100

    Literal escapes

--- Server/Parse.php
+++ Server/Parse.php
@@ -43,8 +43,8 @@ class Parse
 
         // the original c source expands \n \r \t
         // but I'm not...
-        $str = str_replace(array("\n", "\r"), '', $str);
-        $str = str_replace(array("\t", "\r"), '  ', $str);
+        $str = str_replace(array('\n', '\r'), '', $str);
+        $str = str_replace(array('\t', '\r'), '  ', $str);
 
         $length = strlen($str);
         for ($i = 0; $i < $length; $i++) {
---
+++ run.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+#
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+WS=/mnt/www/repos/php_lcdproc/Server/Drivers/WebSocket/cli_server.php
+
+DAEMON=/mnt/www/repos/php_lcdproc/cli_server.php
+NAME="phpLCDd"
+DESC="phpLCDd"
+
+php -f $WS > /dev/null 2>&1 &
+sleep 1
+php -f $DAEMON > /dev/null 2>&1 &
+
+exit 0;
